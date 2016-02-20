a958a8e5199c9ab087d10967c02727a5020a1540
Cindy Fan Chiang <hfanc001@ucr.edu>
2016-02-19 22:51:21 -0800

    Update process.h

--- pintos/src/userprog/process.h
+++ pintos/src/userprog/process.h
@@ -2,6 +2,28 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+#include <list.h>
+#include "threads/synch.h"
+#include "filesys/file.h"
+#include "lib/kernel/list.h"
+
+struct pair
+  {
+    int fd;
+    struct file *file;
+    struct list_elem elem;
+  };
+
+struct exec_helper
+  {
+    const char file_name[16];   /* first part form cmd_line */
+    char file_name_[16];
+    size_t fn_length;           /* length of file_name */
+    const char *cmd_line;       /* whole command line entry */
+    struct semaphore semaphore;
+    bool load_success;
+  };
+
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
